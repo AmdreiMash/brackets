ef12998b635f9224885935096cd92468715373cd
AmdreiMash <100795104+AmdreiMash@users.noreply.github.com>
2022-07-27 21:46:28 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	//const check = (str, bracketsConfig) => {
+	
+	let s = '';
+	let bArry = [];
+	let checkArry =[]
+	
+	
+	bracketsConfig.forEach(elem => {
+		checkArry.push(elem.join(''))
+	})
+	
+	bracketsConfig.forEach(arry => {
+		if(arry[0] == arry[1]){
+			arry.pop()
+		}
+		arry.forEach(e => {
+		bArry.push(e)
+	});
+});
+
+	//console.log(bArry)
+	//console.log(checkArry)
+
+		str.split('').forEach(sE =>{
+			bArry.forEach(cE =>{
+			if (cE == sE){
+				s += sE
+			}
+		})
+	})
+	
+	
+	//console.log(s)
+
+	let strArry = []
+	
+	strArry.push(s)
+	
+	let lenValue = ''
+	
+	do {
+		lenValue = strArry[0].length
+		checkArry.forEach(c => {
+		if(strArry[0].includes(c)){
+			strArry[0] = strArry[0].replace(c, '')
+	}
+	})
+
+	} while (lenValue != strArry[0].length)
+	if (lenValue == 0){
+		return true
+	} else {return false}
 }
+
+//console.log(check('|(|)', [['(', ')'], ['|', '|']]))
+
+
